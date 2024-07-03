feb9ea69f40d036ea1fc9bb823f53f84320146c1
ADITYA092AC <99197595+ADITYA092AC@users.noreply.github.com>
2024-07-03 15:17:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,6 @@ GEMINI_API_KEY=st.secrets["GEMINI_API_KEY"]
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 def get_pdf_text(pdf_docs):
     
